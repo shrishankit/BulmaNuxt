edfba0a77729b21dea04710d55869f4eb4734553
Shrish Ankit <shrish.ankit@qbrics.com>
2019-05-05 20:21:37 +0530

    Apex chart added

---
+++ components/tablelist/tablelist.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="tablelist">
+    <div class="card events-card">
+      <header class="card-header">
+        <p class="card-header-title">Events</p>
+        <a href="#" class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="fa fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </a>
+      </header>
+      <div class="card-table">
+        <div class="content">
+          <table class="table is-fullwidth is-striped">
+            <tbody>
+              <tr>
+                <td width="5%">
+                  <i class="fa fa-bell-o"></i>
+                </td>
+                <td>Lorum ipsum dolem aire</td>
+                <td>
+                  <a class="button is-small is-danger" href="#">Action</a>
+                </td>
+                <td>
+                  <a class="button is-small is-primary" href="#">View</a>
+                </td>
+              </tr>
+              <tr>
+                <td width="5%">
+                  <i class="fa fa-bell-o"></i>
+                </td>
+                <td>Lorum ipsum dolem aire</td>
+                <td>
+                  <a class="button is-small is-primary" href="#">Action</a>
+                </td>
+              </tr>
+              <tr>
+                <td width="5%">
+                  <i class="fa fa-bell-o"></i>
+                </td>
+                <td>Lorum ipsum dolem aire</td>
+                <td>
+                  <a class="button is-small is-primary" href="#">Action</a>
+                </td>
+              </tr>
+              <tr>
+                <td width="5%">
+                  <i class="fa fa-bell-o"></i>
+                </td>
+                <td>Lorum ipsum dolem aire</td>
+                <td>
+                  <a class="button is-small is-primary" href="#">Action</a>
+                </td>
+              </tr>
+              <tr>
+                <td width="5%">
+                  <i class="fa fa-bell-o"></i>
+                </td>
+                <td>Lorum ipsum dolem aire</td>
+                <td>
+                  <a class="button is-small is-primary" href="#">Action</a>
+                </td>
+              </tr>
+              <tr>
+                <td width="5%">
+                  <i class="fa fa-bell-o"></i>
+                </td>
+                <td>Lorum ipsum dolem aire</td>
+                <td>
+                  <a class="button is-small is-primary" href="#">Action</a>
+                </td>
+              </tr>
+              <tr>
+                <td width="5%">
+                  <i class="fa fa-bell-o"></i>
+                </td>
+                <td>Lorum ipsum dolem aire</td>
+                <td>
+                  <a class="button is-small is-primary" href="#">Action</a>
+                </td>
+              </tr>
+              <tr>
+                <td width="5%">
+                  <i class="fa fa-bell-o"></i>
+                </td>
+                <td>Lorum ipsum dolem aire</td>
+                <td>
+                  <a class="button is-small is-primary" href="#">Action</a>
+                </td>
+              </tr>
+              <tr>
+                <td width="5%">
+                  <i class="fa fa-bell-o"></i>
+                </td>
+                <td>Lorum ipsum dolem aire</td>
+                <td>
+                  <a class="button is-small is-primary" href="#">Action</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">View All</a>
+      </footer>
+    </div>
+  </div>
+</template>
