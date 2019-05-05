2213ada76c8d54ffa7a2216f1672ac89c75056f0
Shrish Ankit <shrish.ankit@qbrics.com>
2019-05-05 23:41:45 +0530

    Table added

--- components/charts/linearea.vue
+++ components/charts/linearea.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="lineareachart">
-           <div class="card events-card">
+  <div class="lineareachart">
+    <div class="card events-card round7">
       <header class="card-header">
         <p class="card-header-title">Chart</p>
         <a href="#" class="card-header-icon" aria-label="more options">
@@ -11,59 +11,65 @@
       </header>
       <div class="card-table">
         <div class="content">
-            <div id="chart" class="chartpadding">
-                <apexchart type="area" height="350" :options="chartOptions" :series="series"/>
-              </div>
+          <div id="chart" class="chartpadding">
+            <apexchart type="area" height="350" :options="chartOptions" :series="series"/>
+          </div>
         </div>
       </div>
       <footer class="card-footer">
         <a href="#" class="card-footer-item">View All</a>
       </footer>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-import apexchart from 'vue-apexcharts'
+import apexchart from "vue-apexcharts";
 
 export default {
-    components:{
-       apexchart 
-    },
-    data() {
-        return {
-              series: [{
-          name: 'series1',
+  components: {
+    apexchart
+  },
+  data() {
+    return {
+      series: [
+        {
+          name: "series1",
           data: [31, 40, 28, 51, 42, 109, 100]
-        }, {
-          name: 'series2',
+        },
+        {
+          name: "series2",
           data: [11, 32, 45, 32, 34, 52, 41]
-        }],
-        chartOptions: {
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            curve: 'smooth'
-          },
+        }
+      ],
+      chartOptions: {
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: "smooth"
+        },
 
-
-          xaxis: {
-            type: 'datetime',
-            categories: ["2018-09-19T00:00:00", "2018-09-19T01:30:00", "2018-09-19T02:30:00",
-              "2018-09-19T03:30:00", "2018-09-19T04:30:00", "2018-09-19T05:30:00",
-              "2018-09-19T06:30:00"
-            ],
-          },
-          tooltip: {
-            x: {
-              format: 'dd/MM/yy HH:mm'
-            },
-
+        xaxis: {
+          type: "datetime",
+          categories: [
+            "2018-09-19T00:00:00",
+            "2018-09-19T01:30:00",
+            "2018-09-19T02:30:00",
+            "2018-09-19T03:30:00",
+            "2018-09-19T04:30:00",
+            "2018-09-19T05:30:00",
+            "2018-09-19T06:30:00"
+          ]
+        },
+        tooltip: {
+          x: {
+            format: "dd/MM/yy HH:mm"
           }
         }
-        }
-    },
-}
+      }
+    };
+  }
+};
 </script>
 
